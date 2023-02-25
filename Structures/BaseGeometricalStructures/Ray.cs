e628d758698b272ac94b4611f556de8123b205dd
Oleksandr <OlexandrTanasienko@gmail.com>
2023-02-25 22:23:39 +0200

    Rename Ray starting point

--- Structures/BaseGeometricalStructures/Ray.cs
+++ Structures/BaseGeometricalStructures/Ray.cs
@@ -2,32 +2,32 @@
 
 public struct Ray
 {
-    public Point StartPoint { get; private set; }
+    public Point Origin { get; private set; }
     public Vector Direction { get; private set; }
 
     public Ray(Point start, Vector direction)
     {
-        StartPoint = start;
+        Origin = start;
         Direction = direction;
     }
     
     public Ray(Point start, Point direction)
     {
-        StartPoint = start;
+        Origin = start;
         Direction = new Vector(start, direction);
     }
     
     public Ray(Ray original)
     {
-        StartPoint = original.StartPoint;
+        Origin = original.Origin;
         Direction = original.Direction;
     }
 
     public Point GetNearestPointTo(Point another)
     {
-        Vector vectorToPoint = new Vector(StartPoint, another);
+        Vector vectorToPoint = new Vector(Origin, another);
         float coef = vectorToPoint.DotProductWith(Direction) / (float)Math.Pow(Direction.GetModule(), 2);
-        Point nearest = StartPoint + Direction * coef;
+        Point nearest = Origin + Direction * coef;
 
         return nearest;
     }
