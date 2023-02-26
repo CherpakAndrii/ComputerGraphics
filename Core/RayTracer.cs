753169f07712a0184f0469c71e2cd07550b59551
DjPetuhe <validhernufKPI@gmail.com>
2023-02-26 02:05:52 +0200

    Add Project Render app and fix to ray tracer required and naming

--- Core/RayTracer.cs
+++ Core/RayTracer.cs
@@ -1,35 +1,35 @@
 ﻿using Core.Lights;
 using Core.Scenes;
-using Structures.BaseGeometricalStructures;
 using Structures.Interfaces;
+using Structures.BaseGeometricalStructures;
 
 namespace Core;
    
 public class RayTracer
 {
-    public required Scene scene { get; set; }
+    public Scene Scene { get; set; }
 
-    public RayTracer(Scene scene) { this.scene = scene; }
+    public RayTracer(Scene scene) { Scene = scene; }
 
     public Color[,] TraceRays()
     {
-        Point[,] projectionPlane = scene.Camera.ProjectionPlane;
+        Point[,] projectionPlane = Scene.Camera.ProjectionPlane;
         Color[,] pixels = new Color[projectionPlane.GetLength(0), projectionPlane.GetLength(1)];
         for (int i = 0; i < projectionPlane.GetLength(0); i++)
         {
             for (int j = 0; j < projectionPlane.GetLength(1); j++)
             {
                 pixels[i, j] = new();
-                Ray ray = new(scene.Camera.Position, projectionPlane[i, j]);
+                Ray ray = new(Scene.Camera.Position, projectionPlane[i, j]);
                 if (FindClosestIntersection(ray, out Point intersectionPoint, out IIntersectable? figure))
                 {
-                    for (int k = 0; j < scene.LightSources.Count; k++)
+                    for (int k = 0; j < Scene.LightSources.Count; k++)
                     {
-                        Ray toLightRay = new(intersectionPoint, scene.LightSources[k].GetVector(intersectionPoint) * -1);
+                        Ray toLightRay = new(intersectionPoint, Scene.LightSources[k].GetVector(intersectionPoint) * -1);
                         if (IsOnLight(toLightRay))
                         {
                             Vector normal = figure!.GetNormalVector(intersectionPoint);
-                            if (normal.FindCos(scene.Camera.Direction) > 0) normal *= -1;
+                            if (normal.FindCos(Scene.Camera.Direction) > 0) normal *= -1;
                             double cosLight = toLightRay.Direction.FindCos(normal);
                             if (cosLight > 0)
                             {
@@ -51,16 +51,16 @@ public class RayTracer
         intersection = new();
         figure = default;
         double minDistance = double.MaxValue;
-        for (int i = 0; i < scene.Figures.Count; i++)
+        for (int i = 0; i < Scene.Figures.Count; i++)
         {
-            if (scene.Figures[i].HasIntersectionWith(ray, out Point currIntersection))
+            if (Scene.Figures[i].HasIntersectionWith(ray, out Point currIntersection))
             {
                 intersected = true;
                 double currDistance = new Vector(ray.Origin, currIntersection).GetModule();
                 if (currDistance < minDistance)
                 {
                     intersection = currIntersection;
-                    figure = scene.Figures[i];
+                    figure = Scene.Figures[i];
                     minDistance = currDistance;
                 }
             }
@@ -70,9 +70,9 @@ public class RayTracer
 
     private bool IsOnLight(Ray ray)
     {
-        for (int i = 0; i < scene.Figures.Count; i++)
+        for (int i = 0; i < Scene.Figures.Count; i++)
         {
-            if (scene.Figures[i].HasIntersectionWith(ray, out _)) return false;
+            if (Scene.Figures[i].HasIntersectionWith(ray, out _)) return false;
         }
         return true;
     }
