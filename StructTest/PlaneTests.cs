08c96a800a576bd4a9b4ce1655d56da7eefb4cd5
Oleksandr <OlexandrTanasienko@gmail.com>
2023-03-07 23:49:04 +0200

    Add test for plane normal method

---
+++ StructTest/PlaneTests.cs
@@ -0,0 +1,16 @@
+﻿namespace StructTest;
+
+public class PlaneTests
+{
+    [Test]
+    public void Plane_WhenCallNormalMethod_NormalVectorReceived()
+    {
+        Plane plane = new(new Point(0, 0, 0), new Vector(0, 0, 1));
+        Vector expectedNormal = new(0, 0, 1);
+        Point pointForNormal = new(0, 0, 0);
+
+        var actualNormal = plane.GetNormalVector(pointForNormal);
+        
+        Assert.That(actualNormal, Is.EqualTo(expectedNormal));
+    }
+}
