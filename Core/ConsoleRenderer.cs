7d7cef2e84971aec63d68ecb0b9f7d0567d13d60
Oleksandr <OlexandrTanasienko@gmail.com>
2023-03-07 03:07:16 +0200

    Use ternary operator, replace multiple if statements with switch expression

--- Core/ConsoleRenderer.cs
+++ Core/ConsoleRenderer.cs
@@ -27,11 +27,14 @@ public static class ConsoleRenderer
     private static char ColorToSymbol(Color color)
     {
         int mostIntenseColor = int.Max(color.R, int.Max(color.G, color.B));
-        if (mostIntenseColor <= 5) return ' ';
-        if (mostIntenseColor <= 50) return '.';
-        if (mostIntenseColor <= 125) return '*';
-        if (mostIntenseColor <= 205) return 'O';
-        else return '#';
+        return mostIntenseColor switch
+        {
+            <= 5 => ' ',
+            <= 50 => '.',
+            <= 125 => '*',
+            <= 205 => 'O',
+            _ => '#'
+        };
     }
 
     private static ConsoleColor GetConsoleColor(Color color)
@@ -39,18 +42,15 @@ public static class ConsoleRenderer
         if (color.R >= 160 && color.B >= 160 && color.G >= 160) return ConsoleColor.White;
         if (color.R - color.B > 100 && color.R - color.G > 100)
         {
-            if (color.R >= 150) return ConsoleColor.Red;
-            return ConsoleColor.DarkRed;
+            return color.R >= 150 ? ConsoleColor.Red : ConsoleColor.DarkRed;
         }
         if (color.B - color.R > 100 && color.B - color.G > 100)
         {
-            if (color.B >= 150) return ConsoleColor.Blue;
-            return ConsoleColor.DarkBlue;
+            return color.B >= 150 ? ConsoleColor.Blue : ConsoleColor.DarkBlue;
         }
         if (color.G - color.R > 100 && color.G - color.B > 100)
         {
-            if (color.G >= 150) return ConsoleColor.Green;
-            return ConsoleColor.DarkGreen;
+            return color.G >= 150 ? ConsoleColor.Green : ConsoleColor.DarkGreen;
         }
         if (color.R >= 50 && color.B >= 50 && color.G >= 50) return ConsoleColor.Gray;
         return ConsoleColor.DarkGray;
