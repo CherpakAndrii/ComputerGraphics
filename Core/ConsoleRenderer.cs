f83bc14febc3399854c44dac7094865c11f940dd
Oleksandr <OlexandrTanasienko@gmail.com>
2023-03-07 03:08:52 +0200

    Use merge pattern, fix ternary operators formatting

--- Core/ConsoleRenderer.cs
+++ Core/ConsoleRenderer.cs
@@ -39,20 +39,28 @@ public static class ConsoleRenderer
 
     private static ConsoleColor GetConsoleColor(Color color)
     {
-        if (color.R >= 160 && color.B >= 160 && color.G >= 160) return ConsoleColor.White;
+        if (color is { R: >= 160, B: >= 160, G: >= 160 })
+            return ConsoleColor.White;
         if (color.R - color.B > 100 && color.R - color.G > 100)
         {
-            return color.R >= 150 ? ConsoleColor.Red : ConsoleColor.DarkRed;
+            return color.R >= 150
+                ? ConsoleColor.Red
+                : ConsoleColor.DarkRed;
         }
         if (color.B - color.R > 100 && color.B - color.G > 100)
         {
-            return color.B >= 150 ? ConsoleColor.Blue : ConsoleColor.DarkBlue;
+            return color.B >= 150
+                ? ConsoleColor.Blue
+                : ConsoleColor.DarkBlue;
         }
         if (color.G - color.R > 100 && color.G - color.B > 100)
         {
-            return color.G >= 150 ? ConsoleColor.Green : ConsoleColor.DarkGreen;
+            return color.G >= 150
+                ? ConsoleColor.Green
+                : ConsoleColor.DarkGreen;
         }
-        if (color.R >= 50 && color.B >= 50 && color.G >= 50) return ConsoleColor.Gray;
-        return ConsoleColor.DarkGray;
+        return color is { R: >= 50, B: >= 50, G: >= 50 }
+            ? ConsoleColor.Gray
+            : ConsoleColor.DarkGray;
     }
 }
