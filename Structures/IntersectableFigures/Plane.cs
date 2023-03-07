fdaab9444267aa49912ab3e31057f7d0e534e02e
Oleksandr <OlexandrTanasienko@gmail.com>
2023-03-07 03:28:36 +0200

    Create empty plane class which implements IIntersectable

---
+++ Structures/IntersectableFigures/Plane.cs
@@ -0,0 +1,22 @@
+﻿using Structures.BaseGeometricalStructures;
+using Structures.Interfaces;
+
+namespace Structures.IntersectableFigures;
+
+public class Plane : IIntersectable
+{
+    public Vector GetNormalVector(Point point)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Point? GetIntersectionWith(Ray ray)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool HasIntersectionWith(Ray ray)
+    {
+        throw new NotImplementedException();
+    }
+}
