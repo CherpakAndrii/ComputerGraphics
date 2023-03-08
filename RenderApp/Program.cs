c8a4364cef60ae301f8dc904d8f95424b9a3708e
Oleksandr <OlexandrTanasienko@gmail.com>
2023-03-08 21:27:45 +0200

    Move basic scene to separate method and use it

--- RenderApp/Program.cs
+++ RenderApp/Program.cs
@@ -1,38 +1,10 @@
 ﻿using Core;
 using RenderApp;
-using Core.Lights;
-using Core.Scenes;
-using Core.Cameras;
-using Structures.BaseGeometricalStructures;
-using Structures.IntersectableFigures;
 
-ConsoleConfigurator configuerer = new();
-configuerer.SetupConsole();
+ConsoleConfigurator configurator = new();
+configurator.SetupConsole();
 
-Camera camera = new
-(
-    new Point(0, 0, 0),
-    new Vector(1, 0, 0),
-    90,
-    1
-);
-
-var projectionPlane = new ProjectionPlane(camera, 60, 60);
-
-Sphere sphere = new(new Point(7, 0, 0), 4);
-var plane = new Plane(new Point(10, 10, 0), new Vector(-1, -1, 0));
-var disk = new Disk(new Point(2, 2, 2), new Vector(-1, 0, 0), 1);
-
-LightPoint lightPoint = new(new Point(0, 0, 0), new Color(0, 255, 255));
-LightPoint lightPoint2 = new(new Point(-3, 5, 5), new Color(255, 0, 255));
-
-Scene scene = new() { ProjectionPlane = projectionPlane };
-scene.LightSources.Add(lightPoint);
-scene.LightSources.Add(lightPoint2);
-scene.Figures.Add(sphere);
-scene.Figures.Add(plane);
-scene.Figures.Add(disk);
-
+var scene = ScenesSetup.SceneWithAllFigures();
 
 RayTracer rayTracer = new(scene);
 
--- RenderApp/ScenesSetup.cs
+++ RenderApp/ScenesSetup.cs
@@ -6,9 +6,9 @@ using Structures.IntersectableFigures;
 
 namespace RenderApp;
 
-public class ScenesSetup
+public static class ScenesSetup
 {
-    public Scene CheburashkaScene()
+    public static Scene CheburashkaScene()
     {
         Camera camera = new
         (
@@ -38,6 +38,35 @@ public class ScenesSetup
         scene.Figures.Add(sphere3);
         scene.Figures.Add(sphere4);
         scene.Figures.Add(sphere5);
+
+        return scene;
+    }
+
+    public static Scene SceneWithAllFigures()
+    {
+        Camera camera = new
+        (
+            new Point(0, 0, 0),
+            new Vector(1, 0, 0),
+            90,
+            1
+        );
+
+        var projectionPlane = new ProjectionPlane(camera, 60, 60);
+
+        Sphere sphere = new(new Point(7, 0, 0), 4);
+        var plane = new Plane(new Point(10, 10, 0), new Vector(-1, -1, 0));
+        var disk = new Disk(new Point(2, 2, 2), new Vector(-1, 0, 0), 1);
+
+        LightPoint lightPoint = new(new Point(0, 0, 0), new Color(0, 255, 255));
+        LightPoint lightPoint2 = new(new Point(-3, 5, 5), new Color(255, 0, 255));
+
+        Scene scene = new() { ProjectionPlane = projectionPlane };
+        scene.LightSources.Add(lightPoint);
+        scene.LightSources.Add(lightPoint2);
+        scene.Figures.Add(sphere);
+        scene.Figures.Add(plane);
+        scene.Figures.Add(disk);
 
         return scene;
     }
