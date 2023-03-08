64b62bb49b51c8d5de3f9648034cc5f2a229481f
Oleksandr <OlexandrTanasienko@gmail.com>
2023-03-08 03:48:02 +0200

    Add intersection test for disk

--- StructTest/DiskTests.cs
+++ StructTest/DiskTests.cs
@@ -13,4 +13,43 @@ public class DiskTests
         
         Assert.That(actualNormal, Is.EqualTo(expectedNormal));
     }
+    
+    [TestCaseSource(nameof(_intersectionCases))]
+    public void Disk_WhenIntersectsWithRay_CorrectIntersectionPointReturned
+    (
+        Disk disk,
+        Ray ray,
+        Point expectedIntersectionPoint
+    )
+    {
+        var actualIntersectionPoint = disk.GetIntersectionWith(ray);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(actualIntersectionPoint is not null, Is.True);
+            Assert.That(actualIntersectionPoint, Is.EqualTo(expectedIntersectionPoint));
+        });
+    }
+    
+    private static object[] _intersectionCases =
+    {
+        new object[]
+        {
+            new Disk(new Point(0, 0, 0), new Vector(0, 0, 1), 5),
+            new Ray(new Point(0, 0, 1), new Vector(0, 0, -1)),
+            new Point(0, 0, 0)
+        },
+        new object[]
+        {
+            new Disk(new Point(0, 0, 0), new Vector(0, 0, 1), 5),
+            new Ray(new Point(0, 0, -1), new Vector(0, 0, 1)),
+            new Point(0, 0, 0)
+        },
+        new object[]
+        {
+            new Disk(new Point(0, 0, 0), new Vector(0, 0, 1), 5),
+            new Ray(new Point(0, 4, -1), new Vector(0, 0, 1)),
+            new Point(0, 4, 0)
+        }
+    };
 }
