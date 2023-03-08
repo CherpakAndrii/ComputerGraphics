c8fb7985fc361078cd570f0879f26c811b84e792
Oleksandr <OlexandrTanasienko@gmail.com>
2023-03-08 21:50:57 +0200

    Move methods to separate static class

---
+++ RenderApp/ColorConverter.cs
@@ -0,0 +1,46 @@
+﻿using Core.Lights;
+
+namespace RenderApp;
+
+public static class ColorConverter
+{
+    public static char ColorToSymbol(Color color)
+    {
+        int mostIntenseColor = int.Max(color.R, int.Max(color.G, color.B));
+        return mostIntenseColor switch
+        {
+            <= 5 => ' ',
+            <= 50 => '.',
+            <= 125 => '*',
+            <= 205 => 'O',
+            _ => '#'
+        };
+    }
+
+    public static ConsoleColor GetConsoleColor(Color color, bool retroColor)
+    {
+        if (!retroColor || color is { R: >= 160, B: >= 160, G: >= 160 })
+            return ConsoleColor.White;
+        if (color.R - color.B > 100 && color.R - color.G > 100)
+        {
+            return color.R >= 150
+                ? ConsoleColor.Red
+                : ConsoleColor.DarkRed;
+        }
+        if (color.B - color.R > 100 && color.B - color.G > 100)
+        {
+            return color.B >= 150
+                ? ConsoleColor.Blue
+                : ConsoleColor.DarkBlue;
+        }
+        if (color.G - color.R > 100 && color.G - color.B > 100)
+        {
+            return color.G >= 150
+                ? ConsoleColor.Green
+                : ConsoleColor.DarkGreen;
+        }
+        return color is { R: >= 50, B: >= 50, G: >= 50 }
+            ? ConsoleColor.Gray
+            : ConsoleColor.DarkGray;
+    }
+}
--- RenderApp/ConsoleRenderer.cs
+++ RenderApp/ConsoleRenderer.cs
@@ -19,53 +19,13 @@ public class ConsoleRenderer
             {
                 if (Retro)
                 {
-                    Console.ForegroundColor = GetConsoleColor(pixels[i, j]);
-                    Console.Write(ColorToSymbol(pixels[i, j]));
+                    Console.ForegroundColor = ColorConverter.GetConsoleColor(pixels[i, j], RetroColor);
+                    Console.Write(ColorConverter.ColorToSymbol(pixels[i, j]));
                 }
                 else Console.Write("\x1b[38;2;" + pixels[i, j].R + ";" + pixels[i, j].G + ";" + pixels[i, j].B + "m" + "██");
             }
             Console.Write('\n');
         }
         Console.ForegroundColor = ConsoleColor.White;
-    }
-
-    private static char ColorToSymbol(Color color)
-    {
-        int mostIntenseColor = int.Max(color.R, int.Max(color.G, color.B));
-        return mostIntenseColor switch
-        {
-            <= 5 => ' ',
-            <= 50 => '.',
-            <= 125 => '*',
-            <= 205 => 'O',
-            _ => '#'
-        };
-    }
-
-    private ConsoleColor GetConsoleColor(Color color)
-    {
-        if (!RetroColor || color is { R: >= 160, B: >= 160, G: >= 160 })
-            return ConsoleColor.White;
-        if (color.R - color.B > 100 && color.R - color.G > 100)
-        {
-            return color.R >= 150
-                ? ConsoleColor.Red
-                : ConsoleColor.DarkRed;
-        }
-        if (color.B - color.R > 100 && color.B - color.G > 100)
-        {
-            return color.B >= 150
-                ? ConsoleColor.Blue
-                : ConsoleColor.DarkBlue;
-        }
-        if (color.G - color.R > 100 && color.G - color.B > 100)
-        {
-            return color.G >= 150
-                ? ConsoleColor.Green
-                : ConsoleColor.DarkGreen;
-        }
-        return color is { R: >= 50, B: >= 50, G: >= 50 }
-            ? ConsoleColor.Gray
-            : ConsoleColor.DarkGray;
     }
 }
