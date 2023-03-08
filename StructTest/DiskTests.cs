943ae6efcc8c9f9f23b3bc49abba135f22a415f5
Oleksandr <OlexandrTanasienko@gmail.com>
2023-03-08 03:42:57 +0200

    Add normal test for disk

---
+++ StructTest/DiskTests.cs
@@ -0,0 +1,16 @@
+﻿namespace StructTest;
+
+public class DiskTests
+{
+    [Test]
+    public void Disk_WhenCallNormalMethod_NormalVectorReceived()
+    {
+        Disk plane = new(new Point(0, 0, 0), new Vector(0, 0, 1), 5);
+        Vector expectedNormal = new(0, 0, 1);
+        Point pointForNormal = new(0, 0, 0);
+
+        var actualNormal = plane.GetNormalVector(pointForNormal);
+        
+        Assert.That(actualNormal, Is.EqualTo(expectedNormal));
+    }
+}
