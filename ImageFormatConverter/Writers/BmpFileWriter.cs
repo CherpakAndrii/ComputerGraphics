a540f2956141916033f3324ab7e3817b0fec7a99
CherpakAndrii <a.cherpak-2026@kpi.ua>
2023-03-16 23:33:48 +0200

    Writing to .bmp done

--- ImageFormatConverter/Writers/BmpFileWriter.cs
+++ ImageFormatConverter/Writers/BmpFileWriter.cs
@@ -7,6 +7,85 @@ public class BmpFileWriter : IImageWriter
 {
     public void WriteToFile(string inputFileName, Color[,] pixels)
     {
-        throw new NotImplementedException();
+        int width = pixels.GetLength(0);
+        int height = pixels.GetLength(1);
+        int rowLength = width * 3;
+        int numberOfZeroBytes = 0;
+
+        while (rowLength%4 != 0)
+        {
+            rowLength++;
+            numberOfZeroBytes++;
+        }
+        
+        int fileSize = 54 + height * rowLength;
+
+        string newFileName = GetOutputFileName(inputFileName);
+        byte[] bmpHeader = GetBmpHeader(fileSize);
+        byte[] bmpInfoHeader = GetInfoHeader(width, height);
+        byte[] pixelData = GetPixelData(pixels, numberOfZeroBytes);
+
+        using (BinaryWriter binaryWriter = new BinaryWriter(File.Open(newFileName, FileMode.Create)))
+        {
+            binaryWriter.Write(bmpHeader);
+            binaryWriter.Write(bmpInfoHeader);
+            binaryWriter.Write(pixelData);
+        }
+    }
+
+    private byte[] GetBmpHeader(int fileSize)
+    {
+        byte[] header = new byte[14];
+
+        header[0] = (byte)'B';
+        header[1] = (byte)'M';
+        BitConverter.GetBytes(fileSize).CopyTo(header, 2);
+        BitConverter.GetBytes(54).CopyTo(header, 10);
+
+        return header;
+    }
+    
+    private byte[] GetInfoHeader(int width, int height)
+    {
+        byte[] infoHeader = new byte[40];
+
+        infoHeader[0] = 40;
+        BitConverter.GetBytes(width).CopyTo(infoHeader, 4);
+        BitConverter.GetBytes(height).CopyTo(infoHeader, 8);
+        infoHeader[12] = 1;
+        infoHeader[14] = 24;
+
+        return infoHeader;
+    }
+
+    private byte[] GetPixelData(Color[,] pixels, int zeros)
+    {
+        int width = pixels.GetLength(0);
+        int height = pixels.GetLength(1);
+        int size = height * (width * 3 + zeros);
+        int counter = 0;
+        
+        byte[] pixelData = new byte[size];
+
+        for (int i = height - 1; i >= 0; i--)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                pixelData[counter++] = (byte)pixels[i, j].B;
+                pixelData[counter++] = (byte)pixels[i, j].G;
+                pixelData[counter++] = (byte)pixels[i, j].R;
+            }
+
+            counter += zeros;
+        }
+
+        return pixelData;
+    }
+
+    private string GetOutputFileName(string inputFileName)
+    {
+        string[] splited = inputFileName.Split('.');
+        splited[^1] = "bmp";
+        return String.Join('.', splited);
     }
 }
