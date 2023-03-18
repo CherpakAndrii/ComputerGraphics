48808ea7687b005189fe18f79c1eae4561db74d7
CherpakAndrii <a.cherpak-2026@kpi.ua>
2023-03-18 19:23:33 +0200

    PpmStructureValidator refactored

--- ImageFormatConverter/Validators/PpmStructureValidator.cs
+++ ImageFormatConverter/Validators/PpmStructureValidator.cs
@@ -6,10 +6,17 @@ public class PpmStructureValidator : IImageFileStructureValidator
 {
     public bool ValidateFileStructure(string filename)
     {
+        string firstChars = new StreamReader(filename).ReadLine()!;
+        return firstChars[0] == 'P' && (firstChars[1] == '3' ? ValidateP3Structure(filename) :
+            firstChars[1] == '6' ? ValidateP6Structure(filename) : false);
+    }
+
+    private bool ValidateP3Structure(string filename)
+    {
         string filedata = new StreamReader(filename).ReadToEnd();
         string[] words = filedata.Split(new char[]{' ', '\n', '\r', '\t'}, StringSplitOptions.RemoveEmptyEntries);
         
-        if (words.Length < 7 || words[0].ToLower() != "p3" || !Int32.TryParse(words[1], out int width) ||
+        if (words.Length < 7 || !Int32.TryParse(words[1], out int width) ||
             !Int32.TryParse(words[2], out int height) || !Int32.TryParse(words[3], out int maxColor) || 
             words.Length != width*height*3+4)
             return false;
@@ -21,5 +28,10 @@ public class PpmStructureValidator : IImageFileStructureValidator
         }
 
         return true;
+    }
+    
+    private bool ValidateP6Structure(string filename)
+    {
+        throw new NotImplementedException();
     }
 }
