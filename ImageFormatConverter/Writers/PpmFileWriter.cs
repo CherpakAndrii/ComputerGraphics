588f39944393bbe228b0ccb4aa0021175c115f0b
CherpakAndrii <a.cherpak-2026@kpi.ua>
2023-03-18 23:37:59 +0200

    PpmFileWriter done (just for P)

---
+++ ImageFormatConverter/Writers/PpmFileWriter.cs
@@ -0,0 +1,27 @@
+﻿using Core.Lights;
+using ImageFormatConverter.Interfaces;
+
+namespace ImageFormatConverter.Writers;
+
+public class PpmFileWriter : IImageWriter
+{
+    public void WriteToFile(string outputFileName, Color[,] picture)
+    {
+        StreamWriter streamWriter = new StreamWriter(outputFileName);
+        streamWriter.WriteLine("P3");
+        streamWriter.WriteLine($"{picture.GetLength(1)} {picture.GetLength(0)} 255");
+
+        for (int i = 0; i < picture.GetLength(0); i++)
+        {
+            for (int j = 0; j < picture.GetLength(1); j++)
+            {
+                Color pix = picture[i, j];
+                streamWriter.Write($"{pix.R} {pix.G} {pix.B} ");
+            }
+
+            streamWriter.WriteLine();
+        }
+        
+        streamWriter.Close();
+    }
+}
