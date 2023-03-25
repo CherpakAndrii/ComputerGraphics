656be2c6d45f3eb9b0c337ad6f5d31e41bb58fad
Oleksandr <OlexandrTanasienko@gmail.com>
2023-03-25 23:00:03 +0200

    Load plugins
    Write image plugin dlls to separate directory, load image readers with reflection from dlls

--- ImageFormatConverter.Console/Program.cs
+++ ImageFormatConverter.Console/Program.cs
@@ -1,8 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
+using System.Reflection;
+using ImageFormatConverter.Abstractions.Interfaces;
+
+var allAssemblies = new List<Assembly>();
+const string path = @"C:\Users\Acer\Documents\computerGraphics\ComputerGraphics\ImagePlugins\net7.0";
+
+foreach (var dll in Directory.GetFiles(path, "*.dll"))
+{
+    allAssemblies.Add(Assembly.LoadFile(dll));
+}
+
+var readers = allAssemblies
+    .SelectMany(s => s.GetTypes())
+    .Where(type => typeof(IImageReader) .IsAssignableFrom(type))
+    .ToArray();
 
 foreach (var arg in args)
 {
     Console.WriteLine(arg);
 }
+    
