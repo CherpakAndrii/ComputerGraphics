c3acf2229ac888cb1792e31c30b89f1ac5443014
Oleksandr <OlexandrTanasienko@gmail.com>
2023-03-27 00:54:53 +0300

    Create file writer

---
+++ ImageFormatConverter.Console/FileWriter.cs
@@ -0,0 +1,20 @@
+﻿namespace ImageFormatConverter.Console;
+
+public class FileWriter
+{
+    private readonly string _defaultPath;
+    
+    public FileWriter(string defaultPath)
+    {
+        _defaultPath = defaultPath;
+    }
+
+    public void Write(byte[] targetFileData, string goalFormat, string? outputPath)
+    {
+        var path = outputPath is not null
+            ? outputPath + goalFormat
+            : Path.ChangeExtension(_defaultPath, goalFormat);
+
+        File.WriteAllBytes(path, targetFileData);
+    }
+} 
--- ImageFormatConverter.Console/Program.cs
+++ ImageFormatConverter.Console/Program.cs
@@ -14,8 +14,5 @@ var fileFactory = new FileFactory();
 var fileConverter = fileFactory.CreateFileConverter(fileData, Path.GetExtension(source), flagValues[goalFormatFlag]);
 var targetFileData = fileConverter.ConvertImage(fileData);
 
-var path = flagValues.TryGetValue(outputFlag, out var output)
-    ? output + flagValues[goalFormatFlag]
-    : Path.ChangeExtension(source, flagValues[goalFormatFlag]);
-
-File.WriteAllBytes(path, targetFileData);
+var fileWriter = new FileWriter(source);
+fileWriter.Write(targetFileData, flagValues[goalFormatFlag], flagValues[outputFlag]);
