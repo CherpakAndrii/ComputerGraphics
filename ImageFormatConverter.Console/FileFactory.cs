b33d7378e31d05214dec526f7d8aa0d9fd6f48bb
Oleksandr <OlexandrTanasienko@gmail.com>
2023-03-27 00:39:27 +0300

    Move errors handling of readers and writers to factory

--- ImageFormatConverter.Console/FileFactory.cs
+++ ImageFormatConverter.Console/FileFactory.cs
@@ -29,23 +29,39 @@ public class FileFactory
             .Select(type => (IImageWriter)Activator.CreateInstance(type)!)
             .ToArray();
     }
-    public IImageReader? GetImageReader(byte[] fileData)
+    public IImageReader GetImageReader(byte[] fileData, string? inputExtension)
     {
-        return _imageReaders.FirstOrDefault(reader => reader.ValidateFileStructure(fileData));
+        var reader = _imageReaders.FirstOrDefault(reader => reader.ValidateFileStructure(fileData));
+        if (reader is not null)
+            return reader;
+
+        var supportedReaderFormats = GetSupportedReadersExtensions();
+
+        if (string.IsNullOrEmpty(inputExtension))
+        {
+            throw new Exception($"Error: you are trying to open file with unsupported extension. Only {string.Join(" and ", supportedReaderFormats)} files are supported");
+        }
+        throw new Exception($"Error: you are trying to open {inputExtension} file, but only {string.Join(" and ", supportedReaderFormats)} files are supported");
+        
     }
     
-    public IImageWriter? GetImageWriter(string goalFormat)
+    public IImageWriter GetImageWriter(string goalFormat)
     {
-        return _imageWriters.FirstOrDefault(writer => writer.FileExtension == goalFormat);
+        var writer = _imageWriters.FirstOrDefault(writer => writer.FileExtension == goalFormat);
+        if (writer is not null)
+            return writer;
+        
+        var supportedWriterFormats = GetSupportedWritersExtensions();
+        throw new Exception($"Error: you are trying to write .{goalFormat} file, but only {string.Join(" and ", supportedWriterFormats)} files are supported");
     }
 
-    public string[] GetSupportedReadersExtensions()
+    private string[] GetSupportedReadersExtensions()
     {
-        return _imageReaders.Select(reader => reader.FileExtension).ToArray();
+        return _imageReaders.Select(reader => $".{reader.FileExtension}").ToArray();
     }
-    
-    public string[] GetSupportedWritersExtensions()
+
+    private string[] GetSupportedWritersExtensions()
     {
-        return _imageWriters.Select(reader => reader.FileExtension).ToArray();
+        return _imageWriters.Select(reader => $".{reader.FileExtension}").ToArray();
     }
 }
--- ImageFormatConverter.Console/Program.cs
+++ ImageFormatConverter.Console/Program.cs
@@ -11,27 +11,8 @@ var source = flagValues[sourceFlag];
 var fileData = FileReader.ReadFile(source);
 
 var fileFactory = new FileFactory();
-var targetReader = fileFactory.GetImageReader(fileData);
+var targetReader = fileFactory.GetImageReader(fileData, Path.GetExtension(source));
 var targetWriter = fileFactory.GetImageWriter(flagValues[goalFormatFlag]);
-
-if (targetReader is null)
-{
-    var supportedReaderFormats = fileFactory.GetSupportedReadersExtensions().Select(format => $".{format}");
-    var inputExtension = Path.GetExtension(source);
-    if (string.IsNullOrEmpty(inputExtension))
-    {
-        throw new Exception($"Error: you are trying to open file with unsupported extension. Only {string.Join(" and ", supportedReaderFormats)} files are supported");
-    }
-    throw new Exception($"Error: you are trying to open {inputExtension} file, but only {string.Join(" and ", supportedReaderFormats)} files are supported");
-}
-
-if (targetWriter is null)
-{
-    var supportedWriterFormats = fileFactory.GetSupportedWritersExtensions().Select(format => $".{format}");
-    var goalFormat = flagValues[goalFormatFlag];
-
-    throw new Exception($"Error: you are trying to write .{goalFormat} file, but only {string.Join(" and ", supportedWriterFormats)} files are supported");
-}
 
 var image = targetReader.ImageToPixels(fileData);
 
