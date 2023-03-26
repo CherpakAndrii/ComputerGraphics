998cc0647678ee2357faaaaed49a46d6d2bc47a8
CherpakAndrii <a.cherpak-2026@kpi.ua>
2023-03-26 14:08:28 +0300

    MemoryStream replaced by StringBuilder

--- Writer.PPM/PpmFileWriter.cs
+++ Writer.PPM/PpmFileWriter.cs
@@ -1,4 +1,5 @@
-﻿using Core.Lights;
+﻿using System.Text;
+using Core.Lights;
 using ImageFormatConverter.Abstractions.Interfaces;
 
 namespace Writer.PPM;
@@ -7,24 +8,22 @@ public class PpmFileWriter : IImageWriter
 {
     public byte[] WriteToFile(Color[,] picture)
     {
-        using var stream = new MemoryStream();
-        using var streamWriter = new StreamWriter(stream);
-        streamWriter.WriteLine("P3");
-        streamWriter.WriteLine($"{picture.GetLength(1)} {picture.GetLength(0)} 255");
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.Append("P3\n");
+        stringBuilder.Append($"{picture.GetLength(1)} {picture.GetLength(0)} 255\n");
 
         for (int i = 0; i < picture.GetLength(0); i++)
         {
             for (int j = 0; j < picture.GetLength(1); j++)
             {
                 Color pix = picture[i, j];
-                streamWriter.Write($"{pix.R} {pix.G} {pix.B} ");
+                stringBuilder.Append($"{pix.R} {pix.G} {pix.B} ");
             }
 
-            streamWriter.WriteLine();
+            stringBuilder.Append('\n');
         }
-        
-        stream.Flush();
-        return stream.GetBuffer();
+
+        return Encoding.ASCII.GetBytes(stringBuilder.ToString());
     }
 
     public string FileExtension => "ppm";
