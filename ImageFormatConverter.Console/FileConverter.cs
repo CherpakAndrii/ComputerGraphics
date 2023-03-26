d80e03d83de0dd118501e02932fa5ea6c2676547
Oleksandr <OlexandrTanasienko@gmail.com>
2023-03-27 00:43:34 +0300

    Create FileConverter

---
+++ ImageFormatConverter.Console/FileConverter.cs
@@ -0,0 +1,21 @@
+﻿using ImageFormatConverter.Abstractions.Interfaces;
+
+namespace ImageFormatConverter.Console;
+
+public class FileConverter
+{
+    private IImageWriter _imageWriter;
+    private IImageReader _imageReader;
+    
+    public FileConverter(IImageReader imageReader, IImageWriter imageWriter)
+    {
+        _imageReader = imageReader;
+        _imageWriter = imageWriter;
+    }
+
+    public byte[] ConvertImage(byte[] imageData)
+    {
+        var image = _imageReader.ImageToPixels(imageData);
+        return _imageWriter.WriteToFile(image);
+    }
+}
--- ImageFormatConverter.Console/Program.cs
+++ ImageFormatConverter.Console/Program.cs
@@ -7,16 +7,15 @@ const string outputFlag = "output";
 var flagValues = CommandLineArgumentsParser.GetFlagsValues(args, new []{ goalFormatFlag, sourceFlag }, new []{ outputFlag });
 
 var source = flagValues[sourceFlag];
-
 var fileData = FileReader.ReadFile(source);
 
 var fileFactory = new FileFactory();
 var targetReader = fileFactory.GetImageReader(fileData, Path.GetExtension(source));
 var targetWriter = fileFactory.GetImageWriter(flagValues[goalFormatFlag]);
 
-var image = targetReader.ImageToPixels(fileData);
+var fileConverter = new FileConverter(targetReader, targetWriter);
 
-var targetFileData = targetWriter.WriteToFile(image);
+var targetFileData = fileConverter.ConvertImage(fileData);
 
 var path = flagValues.TryGetValue(outputFlag, out var output)
     ? output + flagValues[goalFormatFlag]
