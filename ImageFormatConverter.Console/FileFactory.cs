5b30bad94379ae8c6d3c7ead15f57432cbffa258
Oleksandr <OlexandrTanasienko@gmail.com>
2023-03-26 22:46:31 +0300

    Create file factory

---
+++ ImageFormatConverter.Console/FileFactory.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using ImageFormatConverter.Abstractions.Interfaces;
+
+namespace ImageFormatConverter.Console;
+
+public class FileFactory
+{
+    public IImageReader? GetImageReader(byte[] fileData)
+    {
+        const string path = @"C:\Users\Acer\Documents\computerGraphics\ComputerGraphics\ImagePlugins\net7.0";
+
+        var allAssemblies = Directory.GetFiles(path, "*.dll")
+            .Select(Assembly.LoadFile).ToList();
+        
+        var readers = allAssemblies
+            .SelectMany(s => s.GetTypes())
+            .Where(type => typeof(IImageReader) .IsAssignableFrom(type))
+            .Select(type => (IImageReader)Activator.CreateInstance(type)!)
+            .ToArray();
+        
+        return readers.FirstOrDefault(reader => reader.ValidateFileStructure(fileData));
+    }
+    
+    public IImageWriter? GetImageWriter(string goalFormat)
+    {
+        const string path = @"C:\Users\Acer\Documents\computerGraphics\ComputerGraphics\ImagePlugins\net7.0";
+
+        var allAssemblies = Directory.GetFiles(path, "*.dll")
+            .Select(Assembly.LoadFile).ToList();
+        
+        var writers = allAssemblies
+            .SelectMany(s => s.GetTypes())
+            .Where(type => typeof(IImageWriter) .IsAssignableFrom(type))
+            .Select(type => (IImageWriter)Activator.CreateInstance(type)!)
+            .ToArray();
+        
+        return writers.FirstOrDefault(writer => writer.FileExtension == goalFormat);
+    }
+}
--- ImageFormatConverter.Console/Program.cs
+++ ImageFormatConverter.Console/Program.cs
@@ -1,26 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-
-using System.Reflection;
-using ImageFormatConverter.Abstractions.Interfaces;
-using ImageFormatConverter.Console;
-
-const string path = @"C:\Users\Acer\Documents\computerGraphics\ComputerGraphics\ImagePlugins\net7.0";
-
-var allAssemblies = Directory.GetFiles(path, "*.dll")
-    .Select(Assembly.LoadFile).ToList();
-
-var readers = allAssemblies
-    .SelectMany(s => s.GetTypes())
-    .Where(type => typeof(IImageReader) .IsAssignableFrom(type))
-    .Select(type => (IImageReader)Activator.CreateInstance(type)!)
-    .ToArray();
-
-var writers = allAssemblies
-    .SelectMany(s => s.GetTypes())
-    .Where(type => typeof(IImageWriter) .IsAssignableFrom(type))
-    .Select(type => (IImageWriter)Activator.CreateInstance(type)!)
-    .ToArray();
+﻿using ImageFormatConverter.Console;
 
 const string goalFormatFlag = "goal-format";
 const string sourceFlag = "source";
@@ -35,9 +13,10 @@ if (!File.Exists(source))
 
 var fileData = File.ReadAllBytes(source);
 
-var targetReader = readers.FirstOrDefault(reader => reader.ValidateFileStructure(fileData));
+var fileFactory = new FileFactory();
 
-var targetWriter = writers.FirstOrDefault(writer => writer.FileExtension == flagValues[goalFormatFlag]);
+var targetReader = fileFactory.GetImageReader(fileData);
+var targetWriter = fileFactory.GetImageWriter(flagValues[goalFormatFlag];
 
 if (targetReader is null)
     throw new Exception("Appropriate file reader is not found");
