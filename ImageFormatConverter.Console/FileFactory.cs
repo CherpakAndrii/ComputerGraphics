0a92e1589f73b342294c6e1bddf4bd589ea5e51d
Oleksandr <OlexandrTanasienko@gmail.com>
2023-03-26 23:44:40 +0300

    Add methods to get lists of supported extensions for readers and writers

--- ImageFormatConverter.Console/FileFactory.cs
+++ ImageFormatConverter.Console/FileFactory.cs
@@ -7,33 +7,45 @@ public class FileFactory
 {
     private const string PluginsPath = "..\\..\\..\\..\\ImagePlugins\\net7.0";
     private const string SearchPattern = "*.dll";
-    public IImageReader? GetImageReader(byte[] fileData)
+
+    private IImageWriter[] _imageWriters;
+    private IImageReader[] _imageReaders;
+
+    public FileFactory()
     {
         var directoryInfo = new DirectoryInfo(PluginsPath);
         var allAssemblies = Directory.GetFiles(directoryInfo.FullName, SearchPattern)
             .Select(Assembly.LoadFile).ToList();
         
-        var readers = allAssemblies
+        _imageReaders = allAssemblies
             .SelectMany(s => s.GetTypes())
             .Where(type => typeof(IImageReader) .IsAssignableFrom(type))
             .Select(type => (IImageReader)Activator.CreateInstance(type)!)
             .ToArray();
         
-        return readers.FirstOrDefault(reader => reader.ValidateFileStructure(fileData));
-    }
-    
-    public IImageWriter? GetImageWriter(string goalFormat)
-    {
-        var directoryInfo = new DirectoryInfo(PluginsPath);
-        var allAssemblies = Directory.GetFiles(directoryInfo.FullName, SearchPattern)
-            .Select(Assembly.LoadFile).ToList();
-        
-        var writers = allAssemblies
+        _imageWriters = allAssemblies
             .SelectMany(s => s.GetTypes())
             .Where(type => typeof(IImageWriter) .IsAssignableFrom(type))
             .Select(type => (IImageWriter)Activator.CreateInstance(type)!)
             .ToArray();
-        
-        return writers.FirstOrDefault(writer => writer.FileExtension == goalFormat);
+    }
+    public IImageReader? GetImageReader(byte[] fileData)
+    {
+        return _imageReaders.FirstOrDefault(reader => reader.ValidateFileStructure(fileData));
+    }
+    
+    public IImageWriter? GetImageWriter(string goalFormat)
+    {
+        return _imageWriters.FirstOrDefault(writer => writer.FileExtension == goalFormat);
+    }
+
+    public string[] GetSupportedReadersExtensions()
+    {
+        return _imageReaders.Select(reader => reader.FileExtension).ToArray();
+    }
+    
+    public string[] GetSupportedWritersExtensions()
+    {
+        return _imageWriters.Select(reader => reader.FileExtension).ToArray();
     }
 }
