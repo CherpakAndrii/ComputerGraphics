3a7724cd79eae0e792fe0cb17d3bb6157f065224
CherpakAndrii <a.cherpak-2026@kpi.ua>
2023-03-27 06:19:25 +0300

    ImageFileWriting tests done

---
+++ ImageFormatConverter.Tests/BmpTests/BmpWritingTests.cs
@@ -0,0 +1,15 @@
+﻿using ImageFormatConverter.Tests.AbstractTests;
+using Writer.BMP;
+
+namespace ImageFormatConverter.Tests.BmpTests;
+
+public class BmpWritingTests : ImageWriting_VisualTests
+{
+    [SetUp]
+    public void Setup()
+    {
+        Destination = TestPicturesDir + "createdBmps/";
+        DefaultExtension = ".bmp";
+        imgWriter = new BmpFileWriter();
+    }
+}
---
+++ ImageFormatConverter.Tests/PpmTests/PpmWritingTests.cs
@@ -0,0 +1,16 @@
+﻿using ImageFormatConverter.Tests.AbstractTests;
+using ImageFormatConverter.Tests.BmpTests;
+using Writer.PPM;
+
+namespace ImageFormatConverter.Tests.PpmTests;
+
+public class PpmWritingTests : ImageWriting_VisualTests
+{
+    [SetUp]
+    public void Setup()
+    {
+        Destination = TestPicturesDir + "createdPpms/"; 
+        DefaultExtension = ".ppm";
+        imgWriter = new PpmFileWriter();
+    }
+}
