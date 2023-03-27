12e2c3433d65d18b3ca1f9cb7ea2c777570e18d7
CherpakAndrii <a.cherpak-2026@kpi.ua>
2023-03-27 04:10:12 +0300

    BmpValidationTests done

--- ImageFormatConverter.Tests/BmpTests/BmpValidationTests.cs
+++ ImageFormatConverter.Tests/BmpTests/BmpValidationTests.cs
@@ -6,6 +6,7 @@ namespace ImageFormatConverter.Tests.BmpTests;
 [TestFixture]
 public class BmpValidationTests
 {
+    private string sourceDir = "../../../testPictures/sources/";
     private BmpFileReader _bmpFileReader;
     private BmpFileWriter _bmpWriter;
     private Color[,] _image;
@@ -26,11 +27,44 @@ public class BmpValidationTests
     }
 
     [Test]
-    public void BMP_ValidateCorrectSample_TrueReturned()
+    public void BMP_ValidateGeneratedPicture_TrueReturned()
     {
         var bytes = _bmpWriter.WriteToFile(_image);
         var structureValidationResult = _bmpFileReader.ValidateFileStructure(bytes);
         
         Assert.That(structureValidationResult, Is.True);
     }
+    
+    [TestCaseSource(nameof(validBmps))]
+    public void BMP_ValidateCorrectSample_TrueReturned(string filename)
+    {
+        var bytes = File.ReadAllBytes(sourceDir+filename);
+        var structureValidationResult = _bmpFileReader.ValidateFileStructure(bytes);
+        
+        Assert.That(structureValidationResult, Is.True);
+    }
+    
+    [TestCaseSource(nameof(invalidBmps))]
+    public void BMP_ValidateIncorrectSample_FalseReturned(string filename)
+    {
+        var bytes = File.ReadAllBytes(sourceDir+filename);
+        var structureValidationResult = _bmpFileReader.ValidateFileStructure(bytes);
+        
+        Assert.That(structureValidationResult, Is.False);
+    }
+    
+    private static object[] validBmps =
+    {
+        new object[] { "correct_sample.bmp" },
+        new object[] { "long_header_sample.bmp" },
+        new object[] { "red_gradient.bmp" },
+        new object[] { "blue_gradient.bmp" },
+        new object[] { "red_blue_gradient.bmp" },
+        new object[] { "pnh.bmp" }
+    };
+    
+    private static object[] invalidBmps =
+    {
+        new object[] { "empty.bmp" }
+    };
 }
