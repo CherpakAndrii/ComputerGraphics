af1ee7fd6d8dee00c941c29badc352b61536c719
CherpakAndrii <a.cherpak-2026@kpi.ua>
2023-04-05 21:23:10 +0300

    Möller–Trumbore intersection algorithm done

--- Structures/IntersectableFigures/Triangle.cs
+++ Structures/IntersectableFigures/Triangle.cs
@@ -33,13 +33,28 @@ public struct Triangle : IIntersectable
     }
 
     public Vector GetNormalVector(Point point) => Normal;
+    
     public Point? GetIntersectionWith(Ray ray)
     {
-        throw new NotImplementedException();
+        const double epsilon = 0.000001;
+        Vector edge1 = new Vector(A, B);
+        Vector edge2 = new Vector(A, C);
+        Vector h = ray.Direction.CrossProductWith(edge2);
+        double a = edge1.DotProductWith(h);
+        if (a > -epsilon && a < epsilon) return null;    // The ray is parallel to the triangle
+        float f = (float)(1.0 / a);
+        Vector s = new Vector(ray.Origin.X - A.X, ray.Origin.Y - A.Y, ray.Origin.Z - A.Z);
+        float u = f * s.DotProductWith(h);
+        if (u < 0.0 || u > 1.0) return null;    // The intersection point is outside the triangle
+        Vector q = s.CrossProductWith(edge1);
+        float v = f * ray.Direction.DotProductWith(q);
+        if (v < 0.0 || u + v > 1.0) return null;    // The intersection point is outside the triangle
+        float t = f * edge2.DotProductWith(q);
+        if (t > epsilon) return ray.Origin + ray.Direction * t;
+        
+        return null;    // The intersection point is behind the ray's origin
     }
+    // implemented by example https://en.wikipedia.org/wiki/M%C3%B6ller%E2%80%93Trumbore_intersection_algorithm
 
-    public bool HasIntersectionWith(Ray ray)
-    {
-        throw new NotImplementedException();
-    }
+    public bool HasIntersectionWith(Ray ray) => GetIntersectionWith(ray) is not null;
 }
