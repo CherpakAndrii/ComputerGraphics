97bb174fbe6284e61ca0a451614277bd369b6e9e
Oleksandr <OlexandrTanasienko@gmail.com>
2023-04-19 00:40:21 +0300

    Start implementing lzw compression

--- Reader.GIF/LZW.cs
+++ Reader.GIF/LZW.cs
@@ -12,7 +12,7 @@ public class Lzw
 	    int endOfInformation = clearCode + 1;
 
 	    List<byte> decompressedData = new();
-	    var dictionary = GetInitializedDictionary(clearCode + 1);
+	    var dictionary = GetInitializedDecompressorDictionary(clearCode + 1);
 	    int digitCapacity = codeSize + 1, index = clearCode + 2, maxIndex = clearCode * 2;
 		string codeWord = string.Empty, previousCodeWord = string.Empty;
 
@@ -30,7 +30,7 @@ public class Lzw
 				
 				if (codeWordInt == clearCode)
 				{
-					dictionary = GetInitializedDictionary((int)Math.Pow(2, codeSize) + 1);
+					dictionary = GetInitializedDecompressorDictionary((int)Math.Pow(2, codeSize) + 1);
 					index = clearCode + 2;
 					maxIndex = clearCode * 2;
 					digitCapacity = codeSize + 1;
@@ -76,13 +76,69 @@ public class Lzw
 
 		return decompressedData.ToArray();
     }
+
+	public byte[] Сompress(IEnumerable<byte> decompressedData, int codeSize)
+	{
+		int clearCode = (int)Math.Pow(2, codeSize);
+		int endOfInformation = clearCode + 1;
+		int digitCapacity = codeSize + 1, index = clearCode + 2, maxIndex = clearCode * 2;
+		
+		var decompressedDataList = decompressedData.ToList();
+		
+		var dictionary = GetInitializedCompressorDictionary(clearCode + 1);
+
+		var currentlyRecognised = ((char)decompressedDataList[0]).ToString();
+		decompressedDataList.RemoveAt(0);
+		
+		List<string> stringBinaryEncoding = new();
+		
+		foreach (var decompressedByte in decompressedDataList)
+		{
+			if (dictionary.ContainsKey(currentlyRecognised + (char)decompressedByte))
+			{
+				currentlyRecognised += ((char)decompressedByte).ToString();
+			}
+			else
+			{
+				if (index == maxIndex)
+				{
+					digitCapacity++;
+					maxIndex *= 2;
+				}
+				stringBinaryEncoding.Add(Helper.IntToBinary(dictionary[currentlyRecognised], digitCapacity));
+				dictionary.Add(currentlyRecognised + (char)decompressedByte, index);
+				index++;
+				currentlyRecognised = ((char)decompressedByte).ToString();
+			}
+		}
+		
+		if (index == maxIndex)
+		{
+			digitCapacity++;
+			maxIndex *= 2;
+		}
+		stringBinaryEncoding.Add(Helper.IntToBinary(dictionary[currentlyRecognised], digitCapacity));
+
+		return Array.Empty<byte>();
+	}
 	
-	private static Dictionary<int, string> GetInitializedDictionary(int maxSize)
+	private static Dictionary<int, string> GetInitializedDecompressorDictionary(int maxSize)
 	{
 		Dictionary<int, string> dictionary = new();
 		for (int i = 0; i <= maxSize - 2; i++)
 		{
 			dictionary.Add(i, Convert.ToChar(i).ToString());
+		}
+
+		return dictionary;
+	}
+
+	private static Dictionary<string, int> GetInitializedCompressorDictionary(int maxSize)
+	{
+		Dictionary<string, int> dictionary = new();
+		for (int i = 0; i <= maxSize - 2; i++)
+		{
+			dictionary.Add(Convert.ToChar(i).ToString(), i);
 		}
 
 		return dictionary;
