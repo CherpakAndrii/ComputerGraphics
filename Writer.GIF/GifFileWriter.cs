fbc31c8df9964d2e5e03db97a251695b50a6fbd1
DjPetuhe <validhernufKPI@gmail.com>
2023-04-23 05:11:02 +0300

    fix gif file writer. Now it works

--- Writer.GIF/GifFileWriter.cs
+++ Writer.GIF/GifFileWriter.cs
@@ -14,14 +14,14 @@ public class GifFileWriter : IImageWriter
         GifPalette palette = GifPaletteSelector.GetPalette(pixels);
         byte[] gifSignature = "GIF89a"u8.ToArray();
         byte[] logicalScreen = CreateLogicalScreenDescriptor(pixels, palette);
+        byte[] colorTable = CreateColorTable(palette);
         byte[] imageDescriptor = CreateImageDescriptor(pixels, palette);
-        byte[] localColorTable = CreateLocalColorTable(palette);
         byte[] dataSubBlocks = CreateDataSubBlocks(pixels, palette);
         return gifSignature.Concat(logicalScreen)
+                           .Concat(colorTable)
                            .Concat(imageDescriptor)
-                           .Concat(localColorTable)
                            .Concat(dataSubBlocks)
-                           .Concat(new byte[] { 59 })
+                           .Concat(";"u8.ToArray())
                            .ToArray();
     }
 
@@ -32,10 +32,12 @@ public class GifFileWriter : IImageWriter
         int globalPalettePower = (int)Math.Ceiling(Math.Log2(palette.BaseColors.Length));
         BitArray globalBitPerPixel = new(new int[] { globalPalettePower - 1 });
         BitArray colorResolution = new(new int[] { 7 });
-        byte packedFields = BitConverter.GetBytes(Helper.IntFromBitArray(globalBitPerPixel, 0, 3) + Helper.IntFromBitArray(colorResolution, 4, 7))[0];
+        int pixel = Helper.IntFromBitArray(globalBitPerPixel, 0, 3);
+        int cr = (Helper.IntFromBitArray(colorResolution, 4, 7) << 4);
+        byte packedFields = BitConverter.GetBytes(pixel + (cr << 4) + 128)[0];
         byte[] lastBytes = new byte[]{ packedFields, 0, 0 };
         return logicalWidth.Concat(logicalHeight)
-                                      .Concat(lastBytes).ToArray();
+                           .Concat(lastBytes).ToArray();
     }
 
     private static byte[] CreateImageDescriptor(Color[,] pixels, GifPalette palette)
@@ -47,7 +49,7 @@ public class GifFileWriter : IImageWriter
         byte[] imageHeight = BitConverter.GetBytes(pixels.GetLength(0))[0..2];
         int locallPalettePower = (int)Math.Ceiling(Math.Log2(palette.BaseColors.Length));
         BitArray localBitPerPixel = new(new int[] { locallPalettePower - 1 });
-        byte[] packedFields = { BitConverter.GetBytes(Helper.IntFromBitArray(localBitPerPixel, 0, 3) + 128)[0] };
+        byte[] packedFields = { BitConverter.GetBytes(Helper.IntFromBitArray(localBitPerPixel, 0, 3))[0] };
         return descriptorSeparator.Concat(imageLeft)
                                   .Concat(imageTop)
                                   .Concat(imageWidth)
@@ -55,7 +57,7 @@ public class GifFileWriter : IImageWriter
                                   .Concat(packedFields).ToArray();
     }
 
-    private static byte[] CreateLocalColorTable(GifPalette palette)
+    private static byte[] CreateColorTable(GifPalette palette)
     {
         int ColorNumbers = (int)Math.Pow(2, Math.Ceiling(Math.Log2(palette.BaseColors.Length)));
         byte[] localColorTable = new byte[ColorNumbers * 3];
@@ -67,7 +69,7 @@ public class GifFileWriter : IImageWriter
             localColorTable[j + 2] = BitConverter.GetBytes(palette.BaseColors[i].B)[0];
             j += 3;
         }
-        for (int i = palette.BaseColors.Length; i < ColorNumbers * 3; i++)
+        for (int i = palette.BaseColors.Length * 3; i < ColorNumbers * 3; i++)
         {
             localColorTable[i] = 0;
         }
@@ -80,17 +82,17 @@ public class GifFileWriter : IImageWriter
         byte[] result = { minLzwCode };
         byte[] compressedData = LzwCompress(pixels, palette, minLzwCode);
         int counter = 0;
-        while (compressedData.Length / 256 > counter)
+        while (compressedData.Length / 255 > counter)
         {
             result = result.Concat(new byte[] { 255 })
-                           .Concat(compressedData[(256 * counter)..(256 * (counter + 1))])
+                           .Concat(compressedData[(255 * counter)..(255 * (counter + 1))])
                            .ToArray();
             counter++;
         }
-        int rest = compressedData.Length % 256;
+        int rest = compressedData.Length % 255;
         return result.Concat(new byte[] { BitConverter.GetBytes(rest)[0] })
-                     .Concat(compressedData[(256 * counter)..compressedData.Length])
-                     .Concat(new byte[] { BitConverter.GetBytes(0)[0] }).ToArray();
+                     .Concat(compressedData[(255 * counter)..compressedData.Length])
+                     .Concat(new byte[] { 0 }).ToArray();
     }
 
     private static byte[] LzwCompress(Color[,] pixels, GifPalette palette, int minLzwCode)
